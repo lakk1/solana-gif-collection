09f914e4f5ea537799c7a09ee1cd148d6e72c815
luckylaxman121@gmail.com <luckylaxman121@gmail.com>
2022-03-19 22:41:18 +0530

    solana backend with gif add,delete,upvote API

--- programs/gif-collection/src/lib.rs
+++ programs/gif-collection/src/lib.rs
@@ -4,60 +4,98 @@ use anchor_lang::solana_program::entrypoint::ProgramResult;
 declare_id!("G71mEsqGnahVbikmpUcW1XKGokKYDU6S8zi2muacf6WA");
 
 #[program]
-pub mod gif_collection {
-  use super::*;
-  pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
-    let base_account = &mut ctx.accounts.base_account;
-    base_account.total_gifs = 0;
-    Ok(())
-  }
+pub mod anchor_app {
+    use super::*;
+    pub fn start_stuff_off(_ctx: Context<StartStuffOff>) -> ProgramResult {
+        let base_account = &mut _ctx.accounts.base_account;
+        base_account.total_gifs = 0;
+        Ok(())
+    }
+    pub fn add_gif(_ctx: Context<AddGif>, gif_link: String) -> ProgramResult {
+        let base_account = &mut _ctx.accounts.base_account;
+        let user = &mut _ctx.accounts.user;
+        let item = ItemStruct {
+            gif_link: gif_link.to_string(),
+            user_address: *user.to_account_info().key,
+            votes_count: 0,
+        };
+        base_account.gif_list.push(item);
+        base_account.total_gifs += 1;
+        Ok(())
+    }
+    pub fn delete_gif(_ctx: Context<DelGif>, gif_link: String) -> ProgramResult {
+        let base_account = &mut _ctx.accounts.base_account;
+        let user = &mut _ctx.accounts.user;
 
-  // The function now accepts a gif_link param from the user. We also reference the user from the Context
-  pub fn add_gif(ctx: Context<AddGif>, gif_link: String) -> ProgramResult {
-    let base_account = &mut ctx.accounts.base_account;
-    let user = &mut ctx.accounts.user;
+        if let Some(pos) = base_account
+            .gif_list
+            .iter()
+            .position(|x| x.gif_link == gif_link && x.user_address == *user.to_account_info().key)
+        {
+            base_account.gif_list.remove(pos);
+            base_account.total_gifs -= 1;
+            return Ok(());
+        }
+       return Ok(());
+    }
+    pub fn vote_gif(_ctx: Context<VoteGif>, gif: ItemStruct) -> ProgramResult {
+        let base_account = &mut _ctx.accounts.base_account;
 
-	// Build the struct.
-    let item = ItemStruct {
-      gif_link: gif_link.to_string(),
-      user_address: *user.to_account_info().key,
-    };
-		
-	// Add it to the gif_list vector.
-    base_account.gif_list.push(item);
-    base_account.total_gifs += 1;
-    Ok(())
-  }
+        if let Some(pos) = base_account
+            .gif_list
+            .iter()
+            .position(|x| x.gif_link == gif.gif_link && x.user_address == gif.user_address)
+        {
+            base_account.gif_list[pos].votes_count += 1;
+            return Ok(());
+        }
+        return Ok(());
+    }
 }
 
 #[derive(Accounts)]
 pub struct StartStuffOff<'info> {
-  #[account(init, payer = user, space = 9000)]
-  pub base_account: Account<'info, BaseAccount>,
-  #[account(mut)]
-  pub user: Signer<'info>,
-  pub system_program: Program <'info, System>,
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
-
 // Add the signer who calls the AddGif method to the struct so that we can save it
 #[derive(Accounts)]
 pub struct AddGif<'info> {
-  #[account(mut)]
-  pub base_account: Account<'info, BaseAccount>,
-  #[account(mut)]
-  pub user: Signer<'info>,
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
 }
 
-// Create a custom struct for us to work with.
+#[derive(Accounts)]
+pub struct DelGif<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct VoteGif<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+}
+
 #[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
 pub struct ItemStruct {
     pub gif_link: String,
     pub user_address: Pubkey,
+    pub votes_count: u64,
 }
 
 #[account]
 pub struct BaseAccount {
     pub total_gifs: u64,
-	// Attach a Vector of type ItemStruct to the account.
+    // Attach a Vector of type ItemStruct to the account.
     pub gif_list: Vec<ItemStruct>,
 }
